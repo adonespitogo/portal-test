de6fdded5dfa8db91d741fe99badc9edde3ace26
Adones Pitogo <pitogo.adones@gmail.com>
2021-06-21 23:49:05 +0800

    server host variable

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+// usage:
+// npm start 10.0.0.1:80
 
 var StressTest = require('./StressTest.js')
-
-var server_host = 'localhost:3000'
+var server_host = process.argv[2] || 'localhost:3000'
 
 var test = new StressTest(server_host)
 
