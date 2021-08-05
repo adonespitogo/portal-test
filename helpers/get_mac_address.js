e6e1bf7c9cdb05053b8417920ceb307d9097b3a0
Adones Pitogo <pitogo.adones@gmail.com>
2021-08-06 01:02:02 +0800

    fix get mac

--- helpers/get_mac_address.js
+++ helpers/get_mac_address.js
@@ -3,5 +3,5 @@ var macaddress = require('macaddress');
 
 module.exports = async () => {
   var mac = await macaddress.one()
-  return mac.toUpperCase()
+  return mac.toUpperCase().replace(':', '').match(/.{1,2}/g).join(':')
 }
